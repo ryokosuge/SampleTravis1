58d2e4c25920725460323fa2853e03b09fcebee8
ryokosuge <ryo.kosuge@gmail.com>
2019-01-09 11:46:06 +0900

    feat:	scriptを自前で書いてみた

---
+++ trigger-travis-job.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -f
+BRANCH=$1
+USER=$2
+REPOSITORY=$3
+
+# TravisCI上で動くの前提で環境変数使っているのでローカルで動かす場合は適時書き換えてください
+# TRAVIS_REPO_SLUGN:	jobのbranch
+# TRAVIS_API_TOKEN:	TravisCIのユーザー管理画面から取得可能
+
+LAST_COMMIT_LOG=`git log --oneline -n 1 HEAD`
+MESSAGE="Triggered by upstream build of $TRAVIS_REPO_SLUG commit "$LAST_COMMIT_LOG""
+BODY="{
+\"request\": {
+  \"branch\": \"$BRANCH\",
+  \"message\": \"$MESSAGE\"
+}}"
+
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token ${TRAVIS_API_TOKEN}" \
+  -d "${BODY}" \
+  https://api.travis-ci.com/repo/${USER}%2F${REPOSITORY}/requests
