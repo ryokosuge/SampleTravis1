fd2dd203895f8d34dc1965cab326bd3c6787f0bc
ryokosuge <ryo.kosuge@gmail.com>
2019-01-09 10:54:35 +0900

    first commit

---
+++ SampleTravis1Tests/SampleTravis1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  SampleTravis1Tests.swift
+//  SampleTravis1Tests
+//
+//  Created by RyoKosuge on 2019/01/09.
+//  Copyright © 2019年 Ryo Kosuge. All rights reserved.
+//
+
+import XCTest
+@testable import SampleTravis1
+
+class SampleTravis1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
